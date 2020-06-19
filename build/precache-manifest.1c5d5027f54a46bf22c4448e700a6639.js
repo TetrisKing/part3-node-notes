784caf2c03ce170e4b6ff490b53fcc40fb4b7e71
Unknown <TetrisCurry@gmail.com>
2020-06-19 22:39:48 +0100

    Add part2 notes frontend build folder

---
+++ build/precache-manifest.1c5d5027f54a46bf22c4448e700a6639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a3ebee7ee27db926b395e2c6f788a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c39b65c0773a6ade99b6",
+    "url": "/static/css/main.d624ac01.chunk.css"
+  },
+  {
+    "revision": "653b250f3659e6836ed9",
+    "url": "/static/js/2.f9742221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9742221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39b65c0773a6ade99b6",
+    "url": "/static/js/main.a067e2eb.chunk.js"
+  },
+  {
+    "revision": "dc11ffb1a1fcdaebfb6e",
+    "url": "/static/js/runtime-main.15a3d875.js"
+  }
+]);
---
+++ build/precache-manifest.64fa56b969b378b4d098f25cd0ccd7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fb5a8a95e254af98ea0a4e18d76295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522d75a13163a26f7144",
+    "url": "/static/css/main.d624ac01.chunk.css"
+  },
+  {
+    "revision": "653b250f3659e6836ed9",
+    "url": "/static/js/2.f9742221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9742221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522d75a13163a26f7144",
+    "url": "/static/js/main.3dabdb79.chunk.js"
+  },
+  {
+    "revision": "dc11ffb1a1fcdaebfb6e",
+    "url": "/static/js/runtime-main.15a3d875.js"
+  }
+]);
